b77c00fb121ec0af914fecc8def9f6b628c9ba3d
Flávio Costa <flaviojcostafilho@gmail.com>
2026-03-06 15:27:35 -0300

    feat: Improve program structure

---
+++ src/FCG.Users.WebApi/DependencyInjection/PipelineDependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using FCG.Users.WebApi.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FCG.Users.WebApi.DependencyInjection
+{
+    [ExcludeFromCodeCoverage]
+    public static class PipelineDependencyInjection
+    {
+        public static WebApplication UseWebApiPipeline(this WebApplication app)
+        {
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Application started successfully");
+            logger.LogInformation("Environment: {Environment}", app.Environment.EnvironmentName);
+
+            if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
+            {
+                app.ApplyMigrations();
+                logger.LogInformation("Migrations applied");
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseCustomerExceptionHandler();
+            app.UseGlobalCorrelationId();
+
+            app.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                AllowCachingResponses = false,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+                }
+
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+
+    }
+}
--- src/FCG.Users.WebApi/Program.cs
+++ src/FCG.Users.WebApi/Program.cs
@@ -3,9 +3,6 @@ using FCG.Users.Infrastructure.Auth.DependencyInjection;
 using FCG.Users.Infrastructure.Kafka.DependencyInjection;
 using FCG.Users.Infrastructure.SqlServer.DependencyInjection;
 using FCG.Users.WebApi.DependencyInjection;
-using FCG.Users.WebApi.Extensions;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Diagnostics.CodeAnalysis;
 
 namespace FCG.Users.WebApi
@@ -19,52 +16,19 @@ namespace FCG.Users.WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddHttpContextAccessor();
-
             builder.Services.AddWebApi(builder.Configuration);
+
             builder.Services.AddApplication();
+
             builder.Services.AddKafkaInfrastructure(builder.Configuration);
-            builder.Services.AddInfrastructure(builder.Configuration);
+
+            builder.Services.AddSqlServerInfrastructure(builder.Configuration);
+
             builder.Services.AddAuthInfrastruture(builder.Configuration);
 
             var app = builder.Build();
 
-            var logger = app.Services.GetRequiredService<ILogger<Program>>();
-            logger.LogInformation("Application started successfully");
-            logger.LogInformation("Environment: {Environment}", app.Environment.EnvironmentName);
-
-            if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
-            {
-                app.ApplyMigrations();
-                logger.LogInformation("Migrations applied");
-            }
-
-            app.UseSwagger();
-            app.UseSwaggerUI();
-
-            app.UseCustomerExceptionHandler();
-            app.UseGlobalCorrelationId();
-
-            app.MapHealthChecks("/health", new HealthCheckOptions
-            {
-                AllowCachingResponses = false,
-                ResultStatusCodes =
-                {
-                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
-                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
-                }
-
-            });
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
+            app.UseWebApiPipeline();
 
             app.Run();
         }
