9d3666bca277d239b85d768865b23260dc214973
flaviojcf <flaviojcostafilho@gmail.com>
2026-05-17 11:38:46 -0300

    feat: Add live deploy example

---
+++ src/FCG.Users.WebApi/Controllers/v1/LiveDeployController.cs
@@ -0,0 +1,19 @@
+﻿using FCG.Users.Application.UseCases.Users.Register;
+using FCG.Users.Application.UseCases.Users.UpdatePassword;
+using FCG.Users.WebApi.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCG.Users.WebApi.Controllers.v1
+{
+    public class LiveDeployController(IMediator mediator) : FcgUserBaseController(mediator)
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> LiveDeploy()
+        {
+            return Ok(ApiResponse<string>.SuccesResponse("Live Deploy is working"));
+        }
+    }
+}
